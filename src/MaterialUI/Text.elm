40c03cc721f7ee7cb1895337e5eb64e12fa0c80f
ole-thoeb <43213977+ole-thoeb@users.noreply.github.com>
2020-04-18 14:10:46 +0200

    Text that wraps if too long

--- src/MaterialUI/Text.elm
+++ src/MaterialUI/Text.elm
@@ -1,4 +1,4 @@
-module MaterialUI.Text exposing (view)
+module MaterialUI.Text exposing (view, wrapping)
 
 import Element exposing (..)
 import MaterialUI.Theme as Theme
@@ -10,3 +10,12 @@ view attr displayStr fontscale theme =
     in
     el (attr ++ Theme.fontToAttributes font)
         (text <| Theme.applyCase font.fontcase displayStr)
+
+
+wrapping : List (Element.Attribute msg) -> String -> Theme.Fontscale -> Theme.Theme a ->  Element msg
+wrapping attrs displayStr fontscale theme =
+    let
+        font = Theme.getFont fontscale theme
+    in
+    paragraph (attrs ++ Theme.fontToAttributes font)
+        [ text <| Theme.applyCase font.fontcase displayStr ]
